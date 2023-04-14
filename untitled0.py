502837b5d8b8d812b49325eaa9d8ab6bfb50a428
Sam.E <samsmoke007@gmail.com>
2023-04-14 21:00:19 +0430

    added some classes

--- untitled0.py
+++ untitled0.py
@@ -1,13 +1,13 @@
 class car:
-    def __init__(self,Cost:int,Name,Color,NumberOfDoors:int,CompanyName,ZeroToHundredSpeed:int):
+    def __init__(self,Cost:int,Model,Color,NumberOfDoors:int,CompanyName,ZeroToHundredSpeed:int):
         self.Cost = Cost
-        self.Name = Name
+        self.Model = Model
         self.Color = Color
         self.NumberOfDoors = NumberOfDoors
         self.CompanyName = CompanyName
         self.ZeroToHundredSpeed = ZeroToHundredSpeed
     def Move(self,Name):
-        print(f"{self.Name} is moving ")
+        pass
     def Braking(self,Name):
         print(f"{self.Name} is stopping")
     def Honking(self,Name):
@@ -45,9 +45,42 @@ class Engine:
             print('This engine belongs to a normal car')
         else:
             print("This engine belongs to a race car ")
-    def EngineAllDescription(self):
+    def EngineTotalDescription(self):
         print(f"{self.Power}/n{self.EngineVolume}/n{self.Use}")
-class 
-
-            
-        
+    def EngineIntrestingsFacts(self):
+        pass
+class PetrolEngine(Engine):
+    def __init__(self, NumberOfCylinder, CylinderType, PistonMaterial, CylinderVolume, PowerToWeightRatio,CoolingSystem):
+        super().__init__(NumberOfCylinder, CylinderType, PistonMaterial, CylinderVolume, PowerToWeightRatio)
+        self.CoolingSystem = CoolingSystem
+    def CoolingPower(self,CoolingSystem):
+        if self.CoolingSystem in ("Air-cooled","AirCooled") :
+            print("watch out.Your engine is old and has an old-fashioned cooling system")
+        else:
+            print("Your engine's cooling system is OK")
+    def EngineIntrestingsFacts(self):
+        print("This engine cosume petrol!!!")
+        print("Your engine is first kind which used to make cars. /n this kind of engine has a real effect in increasing global warming")
+class DieselEngine(Engine):
+    def __init__(self, NumberOfCylinder, CylinderType, PistonMaterial, CylinderVolume, PowerToWeightRatio,NumberOfStrake : int):
+        super().__init__(NumberOfCylinder, CylinderType, PistonMaterial, CylinderVolume, PowerToWeightRatio)
+        self.NumberOfStrake = NumberOfStrake
+    def InUse(self,NumberOfStrake):
+        if NumberOfStrake == 2:
+            print("NO one use this trash .change it quickly")
+        else:
+            print("Your engine is just ok, you can use it.")
+    def EngineIntrestingsFacts(self):
+        print("Your engine type can cnsume many things")
+        print("This Engine is the most used engine in the world. /n most tanks in World War II use this kind of engine")
+class ElectricEngine(Engine):
+    def __init__(self, NumberOfCylinder, CylinderType, PistonMaterial, CylinderVolume, PowerToWeightRatio,AmountOfConsumedKWatt : int):
+        super().__init__(NumberOfCylinder, CylinderType, PistonMaterial, CylinderVolume, PowerToWeightRatio)
+        self.AmountOfConsumedkWatt = AmountOfConsumedKWatt
+    def TypeGuess(self,AmountOfConsumedKWatt):
+        if AmountOfConsumedKWatt > 75 :
+            print("wow I guess you have a very modern and expensive car")
+        else:
+            print("Your car is modern,but hoenstly i don't think it's not expensive")
+class Hybrid(car):
+    
